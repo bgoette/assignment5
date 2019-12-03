5925f32ede1f8ebe79fc30a34eb326a48acb4eef
Barney Goette <bgoette@asu.edu>
2019-12-03 15:12:36 -0600

    implemented add/remove occupants

--- src/main/java/patterns/mediator/City.java
+++ src/main/java/patterns/mediator/City.java
@@ -2,7 +2,10 @@ package main.java.patterns.mediator;
 
 import java.util.ArrayList;
 
+import main.java.BaseCharacter;
 import main.java.BaseLocation;
+import main.java.patterns.factory.BaseHero;
+import main.java.patterns.factory.BaseVillain;
 
 public class City extends BaseLocation {
 
@@ -21,4 +24,46 @@ public class City extends BaseLocation {
         
         return locations.size();
     }
+
+    @Override
+    public boolean addOccupant(BaseCharacter occupant) {
+        if (occupant.getClass().equals(BaseVillain.class)) {
+            return villainLairs.get(randy.nextInt(villainLairs.size())).addOccupant(occupant);
+        } else if (occupant.getClass().equals(BaseHero.class)) {
+            return heroBases.get(randy.nextInt(heroBases.size())).addOccupant(occupant);
+        }
+        
+        return false;
+    }
+
+    @Override
+    public boolean removeOccupant(BaseCharacter occupant) {
+        boolean removed = false;
+        
+        if (occupant.getClass().equals(BaseVillain.class)) {
+            for (Lair lair : villainLairs) {
+                removed = lair.removeOccupant(occupant);
+                
+                if (removed) {
+                    break;
+                }
+            }
+            
+            return removed;
+        } else if (occupant.getClass().equals(BaseHero.class)) {
+            for (HeroBase base : heroBases) {
+                removed = base.removeOccupant(occupant);
+                
+                if (removed) {
+                    break;
+                }
+            }
+            
+            return removed;
+        }
+        
+        return false;
+    }
+    
+    
 }
