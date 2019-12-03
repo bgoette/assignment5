fe118e78f611894983e06b96844c3d1bde5464fa
Barney Goette <bgoette@asu.edu>
2019-12-03 14:48:03 -0600

    added add/remove methods

--- src/main/java/BaseLocation.java
+++ src/main/java/BaseLocation.java
@@ -4,7 +4,25 @@ public abstract class BaseLocation {
 
     protected int occupants;
     
+    /**
+     * Getter for the occupant count.
+     * @return The number of occupants.
+     */
     public int getOccupants() {
         return this.occupants;
     }
+    
+    /**
+     * Adds an occupant to this location.
+     * @param occupant The occupant to add
+     * @return True if adding was successful.
+     */
+    public abstract boolean addOccupant(BaseCharacter occupant);
+
+    /**
+     * Removes an occupant from this location.
+     * @param occupant The occupant to add
+     * @return True if adding was successful.
+     */
+    public abstract boolean removeOccupant(BaseCharacter occupant);
 }
