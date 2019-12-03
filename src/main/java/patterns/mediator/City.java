4ae1caa10758a608e5b7be1f3eb18a769223c119
Barney Goette <bgoette@asu.edu>
2019-12-02 23:55:20 -0600

    added bases/lairs to city

--- src/main/java/patterns/mediator/City.java
+++ src/main/java/patterns/mediator/City.java
@@ -1,5 +1,11 @@
 package main.java.patterns.mediator;
 
-public class City {
+import java.util.ArrayList;
 
+import main.java.BaseLocation;
+
+public class City extends BaseLocation {
+
+    private ArrayList<Lair> villainLairs;
+    private ArrayList<HeroBase> heroBases;
 }
