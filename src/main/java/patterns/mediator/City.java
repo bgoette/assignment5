b344064f8ca96c5e22caa806098eb195d1c9bbdc
Barney Goette <bgoette@asu.edu>
2019-12-03 00:07:56 -0600

    implemented getOccupants()

--- src/main/java/patterns/mediator/City.java
+++ src/main/java/patterns/mediator/City.java
@@ -8,4 +8,17 @@ public class City extends BaseLocation {
 
     private ArrayList<Lair> villainLairs;
     private ArrayList<HeroBase> heroBases;
+    
+    public City() {
+        villainLairs = new ArrayList<Lair>();
+        heroBases = new ArrayList<HeroBase>();
+    }
+    
+    @Override
+    public int getOccupants() {
+        ArrayList<BaseLocation> locations = new ArrayList<BaseLocation>(villainLairs);
+        locations.addAll(heroBases);
+        
+        return locations.size();
+    }
 }
