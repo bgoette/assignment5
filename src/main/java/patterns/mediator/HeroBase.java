7eb51ac282a45e0b90ba2c554735083537e5bc1c
Barney Goette <bgoette@asu.edu>
2019-12-03 15:08:50 -0600

    implemented add/remove occupants

--- src/main/java/patterns/mediator/HeroBase.java
+++ src/main/java/patterns/mediator/HeroBase.java
@@ -1,7 +1,50 @@
 package main.java.patterns.mediator;
 
+import java.util.ArrayList;
+
+import main.java.BaseCharacter;
 import main.java.BaseLocation;
+import main.java.patterns.factory.BaseHero;
 
 public class HeroBase extends BaseLocation {
+    
+    private ArrayList<BaseHero> heroes;
+    
+    /**
+     * Default constructor.
+     */
+    public HeroBase() {
+        heroes = new ArrayList<BaseHero>();
+    }
+
+    @Override
+    public boolean addOccupant(BaseCharacter occupant) {
+        if (occupant.getClass().equals(BaseHero.class)) {
+            if (heroes.size() >= 5) {
+                return false;
+            }
+            
+            heroes.add((BaseHero)occupant);
+            this.occupants++;
+            
+            return true;
+        }
+        
+        return false;
+    }
+
+    @Override
+    public boolean removeOccupant(BaseCharacter occupant) {
+        if (occupant.getClass().equals(BaseHero.class)) {
+            if (heroes.contains(occupant)) {
+                heroes.remove((BaseHero)occupant);
+                
+                this.occupants--;
+                return true;
+            }
+        }
+        
+        return false;
+    }
 
 }
--- src/main/java/patterns/mediator/Lair.java
+++ src/main/java/patterns/mediator/Lair.java
@@ -1,7 +1,50 @@
 package main.java.patterns.mediator;
 
+import java.util.ArrayList;
+
+import main.java.BaseCharacter;
 import main.java.BaseLocation;
+import main.java.patterns.factory.BaseVillain;
 
 public class Lair extends BaseLocation {
+    
+    private ArrayList<BaseVillain> villains;
+    
+    /**
+     * Default constructor.
+     */
+    public Lair() {
+        villains = new ArrayList<BaseVillain>();
+    }
+
+    @Override
+    public boolean addOccupant(BaseCharacter occupant) {
+        if (occupant.getClass().equals(BaseVillain.class)) {
+            if (villains.size() >= 5) {
+                return false;
+            }
+            
+            villains.add((BaseVillain)occupant);
+            this.occupants++;
+            
+            return true;
+        }
+        
+        return false;
+    }
+
+    @Override
+    public boolean removeOccupant(BaseCharacter occupant) {
+        if (occupant.getClass().equals(BaseVillain.class)) {
+            if (villains.contains(occupant)) {
+                villains.remove((BaseVillain)occupant);
+                
+                this.occupants--;
+                return true;
+            }
+        }
+        
+        return false;
+    }
 
 }
