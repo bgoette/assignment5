cb8d538ad3fe8847ba0af1c44155d33328dc3d4a
Barney Goette <bgoette@asu.edu>
2019-12-02 19:09:24 -0600

    adjusted packages so gradle works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
-package java.main;
+package main.java;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        
+        System.out.println("Hello world!");
     }
 
 }
