d0a85768a33a26b73aab66b73893da0cf25d62b2
Barney Goette <bgoette@asu.edu>
2019-12-03 15:42:36 -0600

    added description and log method

--- src/main/java/BaseLocation.java
+++ src/main/java/BaseLocation.java
@@ -6,6 +6,19 @@ public abstract class BaseLocation {
 
     protected static Random randy = new Random();
     protected int occupants;
+    protected String description;
+
+    protected void log(String message) {
+        System.out.println(this.description + ": " + message);
+    }
+    
+    /**
+     * Default constructor.
+     * @param description The description of this location
+     */
+    public BaseLocation(String description) {
+        this.description = description;
+    }
     
     /**
      * Getter for the occupant count.
