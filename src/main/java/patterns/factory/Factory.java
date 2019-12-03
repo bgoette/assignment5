4f4fc26a7a9910a7fc62e3b478f4df8543bb0d03
Barney Goette <bgoette@asu.edu>
2019-12-02 23:40:35 -0600

    decided to use one factory for heroes/villains

--- src/main/java/patterns/factory/Factory.java
+++ src/main/java/patterns/factory/Factory.java
@@ -7,10 +7,11 @@ import java.util.Random;
  * @author bagoette
  *
  */
-public class HeroFactory {
+public class Factory {
 
     private static Random randy = new Random();
-    private static int numHeroes = 3;
+    private static int numHeroes = 4;
+    private static int numVillains = 3;
     
     /**
      * Creates a random hero.
@@ -21,16 +22,41 @@ public class HeroFactory {
         
         switch (num) {
             case 0:
-                return new Batman();
+                return new Aquaman();
 
             case 1:
-                return new IronMan();
+                return new Batman();
 
             case 2:
+                return new IronMan();
+
+            case 3:
                 return new Superman();
 
             default:
                 return new Batman();
+        }
+    }
+    
+    /**
+     * Creates a random villain.
+     * @return Returns a random villain.
+     */
+    public static BaseVillain CreateVillain() {
+        int num = randy.nextInt(numVillains);
+        
+        switch (num) {
+            case 0:
+                return new MastermindVillain();
+
+            case 1:
+                return new MonsterVillain();
+
+            case 2:
+                return new NinjaVillain();
+
+            default:
+                return new MastermindVillain();
         }
     }
 }
--- src/main/java/patterns/factory/VillainFactory.java
+++
@@ -1,5 +0,0 @@
-package main.java.patterns.factory;
-
-public class VillainFactory {
-
-}
