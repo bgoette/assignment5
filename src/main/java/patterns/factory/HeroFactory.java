8a3a2ca77b5b53003c97e0203d9081a195c58354
Barney Goette <bgoette@asu.edu>
2019-12-02 23:36:08 -0600

    implemented HeroFactory.java

--- src/main/java/patterns/factory/HeroFactory.java
+++ src/main/java/patterns/factory/HeroFactory.java
@@ -1,5 +1,36 @@
 package main.java.patterns.factory;
 
+import java.util.Random;
+
+/**
+ * Factory for heroes.
+ * @author bagoette
+ *
+ */
 public class HeroFactory {
 
+    private static Random randy = new Random();
+    private static int numHeroes = 3;
+    
+    /**
+     * Creates a random hero.
+     * @return Returns a random hero.
+     */
+    public static BaseHero CreateHero() {
+        int num = randy.nextInt(numHeroes);
+        
+        switch (num) {
+            case 0:
+                return new Batman();
+
+            case 1:
+                return new IronMan();
+
+            case 2:
+                return new Superman();
+
+            default:
+                return new Batman();
+        }
+    }
 }
