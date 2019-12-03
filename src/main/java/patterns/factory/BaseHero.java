7d5f25d93b78536b9f0428127f6dc6c86c9bd38f
Barney Goette <bgoette@asu.edu>
2019-12-02 21:36:31 -0600

    implemented BaseHero.java

--- src/main/java/patterns/factory/BaseHero.java
+++ src/main/java/patterns/factory/BaseHero.java
@@ -4,8 +4,39 @@ import main.java.BaseCharacter;
 
 public abstract class BaseHero extends BaseCharacter {
 
-    public BaseHero(int maxDamage) {
-        super(maxDamage);
+    protected int civiliansSaved;
+    protected int powerLevel;
+    
+    /**
+     * Increases this hero's power level
+     */
+    protected void levelUp() {
+        this.powerLevel++;
     }
 
+    /**
+     * Default constructor
+     * @param maxDamage The max this character can take in damage
+     */
+    public BaseHero(int maxDamage) {
+        super(maxDamage);
+
+        this.civiliansSaved = 0;
+        this.powerLevel = 1;
+    }
+
+    /**
+     * Getter for civiliansSaved
+     * @return Returns the number of civilians this hero saved
+     */
+    public int getCiviliansSaved() {
+        return this.civiliansSaved;
+    }
+    
+    /**
+     * Increases the count of saved civilians
+     */
+    public void saveCivilian() {
+        this.civiliansSaved++;
+    }
 }
