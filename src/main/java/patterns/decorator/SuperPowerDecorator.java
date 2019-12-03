8c744a69a31865fd9de59ffb409221b43d6e36ce
Barney Goette <bgoette@asu.edu>
2019-12-02 22:03:16 -0600

    added damage strength and attack method

--- src/main/java/patterns/decorator/SuperPowerDecorator.java
+++ src/main/java/patterns/decorator/SuperPowerDecorator.java
@@ -1,5 +1,16 @@
 package main.java.patterns.decorator;
 
 public class SuperPowerDecorator implements ISuperPower {
+    
+    protected int damageStrength;
+    
+    public SuperPowerDecorator(int damageStrength) {
+        this.damageStrength = damageStrength;
+    }
+
+    @Override
+    public String attack() {
+        return null;
+    }
 
 }
