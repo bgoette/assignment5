7670388e4ce5d4fc2a0144bed64c1b9449a0eadc
Barney Goette <bgoette@asu.edu>
2019-12-03 19:21:45 -0600

    fixed spotbugs and added power level assignment

--- src/main/java/patterns/factory/BaseVillain.java
+++ src/main/java/patterns/factory/BaseVillain.java
@@ -4,8 +4,14 @@ import main.java.BaseCharacter;
 
 public abstract class BaseVillain extends BaseCharacter {
 
+    /**
+     * Default constructor.
+     * @param maxDamage The max damage this villain can take.
+     * @param description The description of this villain.
+     */
     public BaseVillain(int maxDamage, String description) {
         super(maxDamage, description);
+        
+        this.powerLevel = 1;
     }
-
 }
