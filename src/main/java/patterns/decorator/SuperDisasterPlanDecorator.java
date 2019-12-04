264e7875c1a81079dedb8eae40ed9719f313d651
Barney Goette <bgoette@asu.edu>
2019-12-03 19:39:47 -0600

    added new super power decorators

---
+++ src/main/java/patterns/decorator/SuperDisasterPlanDecorator.java
@@ -0,0 +1,9 @@
+package main.java.patterns.decorator;
+
+public class SuperDisasterPlanDecorator extends SuperPowerDecorator {
+
+    public SuperDisasterPlanDecorator(int damageStrength) {
+        super(damageStrength, "You'll never stop me, HAHAHA!");
+    }
+
+}
