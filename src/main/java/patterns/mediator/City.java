81f674824f66b2ae7805f19667279f261ac82d0d
Barney Goette <bgoette@asu.edu>
2019-12-03 22:26:13 -0600

    added methods to get the remaining heroes/villains count

--- src/main/java/patterns/mediator/City.java
+++ src/main/java/patterns/mediator/City.java
@@ -113,6 +113,26 @@ public class City extends BaseLocation {
 
         this.log(message);
     }
+    
+    private int getTotalVillainsLeft() {
+        int villainsLeft  = 0;
+        
+        for (Lair lair : villainLairs) {
+            villainsLeft += lair.getOccupantCount();
+        }
+        
+        return villainsLeft;
+    }
+    
+    private int getTotalHeroesLeft() {
+        int heroesLeft  = 0;
+        
+        for (HeroBase base : heroBases) {
+            heroesLeft += base.getOccupantCount();
+        }
+        
+        return heroesLeft;
+    }
 
     /**
      * Runs on a set tick timer. This implements most of the requirements
@@ -122,7 +142,9 @@ public class City extends BaseLocation {
         
         // If there are no hero bases left, the villains win
         if (heroBases.size() <= 0) {
-            endSimulation("[MAYHEM] THE VILLAINS WIN!");
+            int villainsLeft = getTotalVillainsLeft();
+            
+            endSimulation("[MAYHEM] THE VILLAINS WIN! " + villainsLeft + " villains left!");
             return;
         }
 
@@ -132,7 +154,9 @@ public class City extends BaseLocation {
 
         // If no heroes left, the villains win
         if (heroes <= 0) {
-            endSimulation("[MAYHEM] THE VILLAINS WIN!");
+            int villainsLeft = getTotalVillainsLeft();
+            
+            endSimulation("[MAYHEM] THE VILLAINS WIN! " + villainsLeft + " villains left!");
             return;
         }
 
@@ -141,7 +165,9 @@ public class City extends BaseLocation {
 
         // If there are no villain lairs left, the heroes win
         if (villainLairs.size() <= 0) {
-            endSimulation("[JOYOUS] THE HEROES WIN!");
+            int heroesLeft = getTotalHeroesLeft();
+            
+            endSimulation("[JOYOUS] THE HEROES WIN! " + heroesLeft + " heroes left!");
             return;
         }
 
@@ -152,7 +178,9 @@ public class City extends BaseLocation {
 
         // If there are no villains left, the heroes win
         if (villains <= 0) {
-            endSimulation("[JOYOUS] THE HEROES WIN!");
+            int heroesLeft = getTotalHeroesLeft();
+            
+            endSimulation("[JOYOUS] THE HEROES WIN! " + heroesLeft + " heroes left!");
             return;
         }
 
@@ -210,15 +238,23 @@ public class City extends BaseLocation {
             hero.heal(randy.nextInt(3) + 1);
         } else {
             if (villain.getIsAlive()) {
+                
                 // Heal for the next 1 to 3 ticks
                 villain.heal(randy.nextInt(3) + 1);
+                
+                // Level up villain for defeating hero
                 villain.levelUp();
 
+                // Clone this villain
                 BaseVillain newVillain = Factory.clone(villain);
 
+                // Try to add clone to this lair
                 boolean addSuccess = lair.addOccupant(newVillain);
 
+                // If couldn't add to this lair...
                 if (!addSuccess) {
+                    
+                    // Create new lair and add villain to that one
                     Lair newLair = new Lair();
                     newLair.addOccupant(newVillain);
 
@@ -226,14 +262,17 @@ public class City extends BaseLocation {
                 }
             }
 
+            // Remove dead hero from base
             heroBase.removeOccupant(hero);
 
+            // If no heroes left in base, it's destroyed and removed from city
             if (heroBase.getOccupantCount() <= 0) {
                 this.log("[COLLAPSE] " + heroBase.toString() + " has been destroyed!");
                 heroBases.remove(heroBase);
             }
         }
 
+        // Run updates for all characters
         for (HeroBase base : heroBases) {
             base.update();
         }
